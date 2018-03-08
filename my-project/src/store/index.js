4f506025fe1de5f1c23985b74e69233a06129ff8
haiyao <1770480683@qq.com>
2018-03-08 12:30:14 +0800

    完成专栏详情页

--- my-project/src/store/index.js
+++ my-project/src/store/index.js
@@ -1,17 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import columnsDetail from './columnsDetail'
 
 Vue.use(Vuex)
 import home from './home'
 import list from './list'
 import homedetail from './homedetail'
 import columns from './columns'
+import columnsDetail from './columnsDetail'
 const store = new Vuex.Store({
     modules:{
         home,list,homedetail,columns,columnsDetail
     }
-
-
 }) 
 export default store
