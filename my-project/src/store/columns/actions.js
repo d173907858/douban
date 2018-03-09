dd654665d5a00f59a0db26dee62ce67db8412ad0
haiyao <1770480683@qq.com>
2018-03-09 19:09:39 +0800

    修复轮播bug

--- my-project/src/store/columns/actions.js
+++ my-project/src/store/columns/actions.js
@@ -6,35 +6,12 @@ import {
 } from './const'
 import axios from 'axios'
 import config from '../../modules/config'
-import Vue from 'vue'
-import Swiper from 'swiper'
 let actions = {
     [GET_COLUMNS] ({commit}) {
         axios.get(config.host + '/db/j/column_v2/').then(res =>{
             commit(SET_BANNERS, res.data.banners)
             commit(SET_CHARTS, res.data.charts)
             commit(SET_KINDS, res.data.kinds)
-            Vue.nextTick(function () {
-                new Swiper('.books', {
-                    slidesPerView : 3,
-                    resistance : false,
-                    centeredSlides : false,
-                    scrollbar: {
-                        el: '.swiper-scrollbar',
-                        hide: true
-                    }
-                })
-                 new Swiper('.columns-banner', {
-                    loop:true,
-                    autoplay: true,
-                    disableOnInteraction: false,
-                    allowTouchMove: false,
-                    pagination: {
-                        el: '.swiper-pagination',
-                        clickable :true,
-                    }
-                })
-            })
         })
     }
 }
